b051e132273f538c3e3f963791cf424385c2f08e
Aaron Latham <alatham7891@gmail.com>
2022-11-29 01:43:37 +0000

    ADDED: Nav menu

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,26 @@
 <script setup>
+import { NConfigProvider } from "naive-ui";
 import { RouterView } from "vue-router";
 import Navbar from "./components/Navbar.vue";
+
+const themeOverrides = {
+   common: {
+      primaryColor: "#ff6347",
+   },
+   Input: {
+      borderHover: "1px solid rgba(255, 99, 71, 0.5)",
+      borderFocus: "1px solid rgba(255, 99, 71, 0.3)",
+   },
+};
 </script>
 
 <template>
-   <main>
-      <Navbar />
-      <RouterView />
-   </main>
+   <NConfigProvider :theme-overrides="themeOverrides">
+      <main>
+         <Navbar />
+         <RouterView />
+      </main>
+   </NConfigProvider>
 </template>
 
 <style lang="scss" scoped>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,32 +1,116 @@
 <script setup>
-import { Bars3Icon } from "@heroicons/vue/24/outline";
+import { ref } from "vue";
+import {
+   Bars3Icon,
+   MagnifyingGlassIcon,
+   HomeIcon,
+   CodeBracketIcon,
+   CommandLineIcon,
+   ChatBubbleBottomCenterIcon,
+} from "@heroicons/vue/24/outline";
+
+import { NInput, NDrawer, NDrawerContent } from "naive-ui";
+
+const searchText = ref("");
+const searchLoading = ref(false);
+
+const menuActive = ref(false);
+
+const menuActivate = () => {
+   menuActive.value = true;
+};
 </script>
 
 <template>
    <nav>
       <div class="nav-right">
-         <Bars3Icon class="icon" />
+         <MagnifyingGlassIcon class="icon-search" />
+         <NInput
+            class="nav-search"
+            v-model:value="searchText"
+            type="text"
+            size="large"
+            placeholder="Search..."
+            :loading="searchLoading"
+         />
+
+         <Bars3Icon class="icon" @click="menuActivate" />
+         <n-drawer v-model:show="menuActive" :width="350" placement="right">
+            <n-drawer-content
+               title="Navigation"
+               :header-style="{ fontSize: '25px' }"
+               closable
+            >
+               <ul class="nav-list">
+                  <li>
+                     <HomeIcon
+                        :style="{ width: '25px', marginRight: '10px' }"
+                     />
+                     Home
+                  </li>
+                  <li>
+                     <CodeBracketIcon
+                        :style="{ width: '25px', marginRight: '10px' }"
+                     />
+                     Projects
+                  </li>
+                  <li>
+                     <CommandLineIcon
+                        :style="{ width: '25px', marginRight: '10px' }"
+                     />
+                     Github
+                  </li>
+                  <li>
+                     <ChatBubbleBottomCenterIcon
+                        :style="{ width: '25px', marginRight: '10px' }"
+                     />
+                     Contact
+                  </li>
+               </ul>
+            </n-drawer-content>
+         </n-drawer>
       </div>
    </nav>
 </template>
 
 <style lang="scss" scoped>
 nav {
-   margin: 0 auto;
+   margin: 0 auto 30px auto;
    max-width: 1440px;
 
    .nav-right {
       display: flex;
       justify-content: end;
+      align-items: center;
+
+      img {
+         margin-right: 20px;
+         width: 35px;
+         cursor: pointer;
+      }
 
       .icon {
-         width: 4%;
+         width: 40px;
          cursor: pointer;
          transition: 500ms ease;
 
          &:hover {
             color: tomato;
          }
+      }
+
+      .icon-search {
+         width: 20px;
+         color: #999;
+         position: absolute;
+         right: 342px;
+         z-index: 2;
+      }
+
+      .nav-search {
+         width: 250px;
+         padding-left: 30px;
+         margin-right: 35px;
       }
    }
 }
